2cab3138ae521a36d54ac7bd29f84da07ce315ef
ksyautumm <sk3333@cornell.edu>
2022-04-15 19:38:22 -0400

    minor change

--- line_search_test.py
+++ line_search_test.py
@@ -13,5 +13,5 @@ def obj_grad(x):
 start_point = np.array([1.8, 1.7])
 search_gradient = np.array([-1.0, -1.0])
 
-print(line_search_strong.line_search(
+# print(line_search_strong.line_search(
     obj_func, obj_grad, start_point, search_gradient))
